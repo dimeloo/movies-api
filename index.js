3d2ca8f1bd2577cee217a835a2ec808675fc452a
Mike Ortiz <dimelo@Mikes-MacBook-Air.local>
2022-08-08 19:55:24 -0400

    updated

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express()
 app.use(cors())
 app.use(express.json()) // Required to receive JSON in body for POST
 
-const PORT = 4000
+// const PORT = 4000
+app.listen(process.env.PORT, () => console.log('API running on ', process.env.PORT))
 
 /* *********************************************************************** */
 // CRUD
@@ -40,4 +41,3 @@ app.put('/update-movie', updateMovie)
 // Employee Routes
 app.post('/add-employee', addEmployee)
 app.get('/', (req, res) => res.send('Hey here is my API!!'))
-app.listen(process.env.PORT, () => console.log('API running on ', process.env.PORT))
